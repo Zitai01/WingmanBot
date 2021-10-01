d56a338d52ff41586d7e9d5b00d72562a84d28bd
Zitai Huang <mywpiedu@gmail.com>
2021-10-01 15:25:42 -0500

    hello

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// Require the necessary discord.js classes
+const { Client, Intents } = require('discord.js')
+require('dotenv').config()
+
+const token = process.env.DISCORD_BOT_TOKEN
+console.log(token)
+// Create a new client instance
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+})
+// Loki
+const guild = '229749241810124801'
+const clientId = '893515753217720320'
+// When the client is ready, run this code (only once)
+client.on('ready', () => {
+  console.log(`${client.user.tag}is ready`)
+})
+client.on('message', (message) => {
+  if (message.author.bot) return
+  console.log(`${message.author}`)
+  if (message.content === 'hello') {
+    message.channel.send('hello there')
+  }
+})
+// Login to Discord with your client's token
+client.login(token)
